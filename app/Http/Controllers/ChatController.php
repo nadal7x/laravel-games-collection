6b5990bfb4e6207e28d119377a4f387e4a96c7d9
nadal7x <nadal7x@hotmail.com>
2026-04-29 13:06:07 +0200

    chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -11,6 +11,7 @@ class ChatController extends Controller
     {
         $client = OpenAI::client(env('OPENAI_API_KEY'));
 
+        /*//// Chat con prompts ////*/
         $response = $client->responses()->create([
             'prompt' => [
                 'id' => 'pmpt_69e89cb2aee88195b733a29feae7f66a044b0f745717813b',
@@ -51,6 +52,35 @@ foreach ($secondResponse->output as $output) {
         }
     }
 }
+
+    // /*//// Chat con assistant ////*/
+
+    // // 1. Crear thread
+    // $thread = $client->threads()->create([]);
+
+    // // 2. Mensaje usuario
+    // $client->threads()->messages()->create($thread->id, [
+    //     'role' => 'user',
+    //     'content' => $request->message,
+    // ]);
+
+    // // 3. Ejecutar assistant
+    // $run = $client->threads()->runs()->create($thread->id, [
+    //     'assistant_id' => 'asst_McrCZNskOzdcJF7EP4f70PhZ',
+    // ]);
+
+    // // 4. Esperar respuesta
+    // do {
+    //     $run = $client->threads()->runs()->retrieve($thread->id, $run->id);
+    //     sleep(1);
+    // } while ($run->status !== 'completed');
+
+    // // 5. Obtener mensajes
+    // $messages = $client->threads()->messages()->list($thread->id);
+
+    // $reply = $messages->data[0]->content[0]->text->value ?? '';
+
+
 
 return response()->json([
     'reply' => $reply
